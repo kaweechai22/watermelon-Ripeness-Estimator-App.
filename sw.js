b958412f2458e1369c0f693f8d513ce1e047ca89
kaweechai22 <cacarosjampa@gmail.com>
2025-07-14 10:53:19 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,14 @@
-const cacheName = 'watermelon-app-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => cache.addAll(filesToCache))
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("watermelon-app").then((cache) =>
+      cache.addAll(["./", "./index.html", "./manifest.json"])
+    )
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
+
